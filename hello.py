09e3e6d1c6ac45a6b375285480a05633ec3a2b3a
bamichalskw <michalskw@users.noreply.github.com>
2021-04-18 19:33:27 +0200

    zmiana imienia

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 
-moje_imie = 'Natalia'
+moje_imie = 'Michal'
 
 @app.route('/')
 def hello_world():
@@ -15,3 +15,7 @@ def kto():
 @app.route('/wiadomosc')
 def wiadomosc():
     return 'Hello, World!'
+
+@app.route('/nowyrok')
+def nowyrok():
+    return '2021!'
